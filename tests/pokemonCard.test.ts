f6d3779af143dc360efd72135856a4aabd980a96
GUERRIER Kevin <guerrier.k@gmail.com>
2026-03-02 14:57:33 +0100

    Ajout tests POST /pokemon-cards

--- tests/pokemonCard.test.ts
+++ tests/pokemonCard.test.ts
@@ -5,48 +5,134 @@ import { prismaMock } from './jest.setup';
 describe('PokemonCard API', () => {
   describe('GET /pokemon-cards', () => {
     it('should fetch all PokemonCards', async () => {
-      const mockPokemonCards = [];
+      const mockPokemonCards = [
+        {
+          id: 1,
+          name: 'Pikachu',
+          pokedexId: 25,
+          typeId: 1,
+          imageUrl: 'pikachu.png',
+          lifePoints: 60,
+          weight: 85,
+          size: 6,
+          attackId: 2,
+          weaknessId: 2,
+        },
+        {
+          id: 2,
+          name: 'Charmander',
+          pokedexId: 4,
+          typeId: 1,
+          imageUrl: 'charmander.png',
+          lifePoints: 50,
+          weight: 85,
+          size: 6,
+          attackId: 2,
+          weaknessId: 2,
+        },
+      ];
 
+      prismaMock.pokemonCard.findMany.mockResolvedValue(mockPokemonCards);
+
+      const response = await request(app).get('/pokemon-cards');
       expect(response.status).toBe(200);
       expect(response.body).toEqual(mockPokemonCards);
+    });
+
+
+    it('should return 500 if fetching fails', async () => {
+      prismaMock.pokemonCard.findMany.mockRejectedValue(
+        new Error('Database error')
+      );
+
+      const response = await request(app).get('/pokemon-cards');
+      expect(response.status).toBe(500);
+      expect(response.body).toEqual({ error: 'Failed to fetch pokemonCards' });
     });
   });
 
   describe('GET /pokemon-cards/:pokemonCardId', () => {
     it('should fetch a PokemonCard by ID', async () => {
-      const mockPokemonCard = {};
+      const mockPokemonCard = {
+        id: 1,
+        name: 'Pikachu',
+        pokedexId: 25,
+        typeId: 1,
+        imageUrl: 'pikachu.png',
+        lifePoints: 60,
+        weight: 85,
+        size: 6,
+        attackId: 2,
+        weaknessId: 2,
+      };
 
+      prismaMock.pokemonCard.findUnique.mockResolvedValue(mockPokemonCard);
+
+      const response = await request(app).get('/pokemon-cards/1');
       expect(response.status).toBe(200);
       expect(response.body).toEqual(mockPokemonCard);
     });
 
     it('should return 404 if PokemonCard is not found', async () => {
+      prismaMock.pokemonCard.findUnique.mockResolvedValue(null);
+
+      const response = await request(app).get('/pokemon-cards/999');
       expect(response.status).toBe(404);
-      expect(response.body).toEqual({ error: 'PokemonCard not found' });
+      expect(response.body).toEqual({ error: 'Failed to find pokemonCard 999' });
+    });
+
+    it('should return 500 if fetching fails', async () => {
+      prismaMock.pokemonCard.findUnique.mockRejectedValue(
+        new Error('Database error')
+      );
+
+      const response = await request(app).get('/pokemon-cards/1');
+      expect(response.status).toBe(500);
+      expect(response.body).toEqual({ error: 'Failed to fetch pokemonCard' });
     });
   });
 
   describe('POST /pokemon-cards', () => {
     it('should create a new PokemonCard', async () => {
-      const createdPokemonCard = {};
+      const newPokemonCard = {
+        name: 'Bulbasaur',
+        pokedexId: 1,
+        typeName: 'Grass',
+        imageUrl: 'bulbasaur.png',
+        lifePoints: 45,
+        attackId: 1,
+        weight: 85,
+        size: 6,
+        weaknessId: 2,
+      };
+      const foundType = { id: 3, name: 'Grass' };
+      const createdPokemonCard = { id: 1, typeId: 3, ...newPokemonCard };
+      
+      prismaMock.type.findUnique.mockResolvedValue(foundType);
+      prismaMock.pokemonCard.create.mockResolvedValue(createdPokemonCard);
+
+      const response = await request(app)
+        .post('/pokemon-cards')
+        .set('Authorization', 'Bearer mockedToken')
+        .send(newPokemonCard);
 
       expect(response.status).toBe(201);
       expect(response.body).toEqual(createdPokemonCard);
     });
   });
 
-  describe('PATCH /pokemon-cards/:pokemonCardId', () => {
-    it('should update an existing PokemonCard', async () => {
-      const updatedPokemonCard = {};
+  // describe('PATCH /pokemon-cards/:pokemonCardId', () => {
+  //   it('should update an existing PokemonCard', async () => {
+  //     const updatedPokemonCard = {};
 
-      expect(response.status).toBe(200);
-      expect(response.body).toEqual(updatedPokemonCard);
-    });
-  });
+  //     expect(response.status).toBe(200);
+  //     expect(response.body).toEqual(updatedPokemonCard);
+  //   });
+  // });
 
-  describe('DELETE /pokemon-cards/:pokemonCardId', () => {
-    it('should delete a PokemonCard', async () => {
-      expect(response.status).toBe(204);
-    });
-  });
+  // describe('DELETE /pokemon-cards/:pokemonCardId', () => {
+  //   it('should delete a PokemonCard', async () => {
+  //     expect(response.status).toBe(204);
+  //   });
+  // });
 });
